ab41fa19ef0fbeb0088d8f325b1dc4280cbe9817
Hamcha <zikyky@gmail.com>
2014-05-24 19:47:03 +0200

    Forgot to make it visible

--- Ketchup/Chunked.hs
+++ Ketchup/Chunked.hs
@@ -8,7 +8,9 @@ module Ketchup.Chunked
 
 import qualified Data.ByteString.Char8 as B
 import qualified Data.List as List
+import           Ketchup.Httpd
 import           Network
+import           Network.Socket.ByteString
 import           Numeric
 
 chunkHeaders :: Socket -> Int -> [(B.ByteString, [B.ByteString])] -> IO ()
@@ -22,11 +24,11 @@ chunkHeaders handle status headers = do
     heads = B.concat $ map (\x -> B.concat [fst x, ": ", B.concat $ List.intersperse "," $ snd x]) headers
 
 
-chunk :: Socket -> B.Bytestring -> IO ()
+chunk :: Socket -> B.ByteString -> IO ()
 chunk handle value =
     sendAll handle content
     where
-    content = B.concat [B.pack $ showHex (length value) "",
+    content = B.concat [B.pack $ showHex (B.length value) "",
                         "\r\n", value, "\r\n"]
 
 endchunk :: Socket -> IO ()
