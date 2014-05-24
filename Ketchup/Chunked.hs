691816444999b3c2b424bdcef7eeb977fa8b18a5
Hamcha <zikyky@gmail.com>
2014-05-24 19:40:54 +0200

    Chunked encoding

---
+++ Ketchup/Chunked.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Ketchup.Chunked
+( chunkHeaders
+, chunk
+, endchunk
+) where
+
+import qualified Data.ByteString.Char8 as B
+import qualified Data.List as List
+import           Network
+import           Numeric
+
+chunkHeaders :: Socket -> Int -> [(B.ByteString, [B.ByteString])] -> IO ()
+chunkHeaders handle status headers = do
+    sendAll handle content
+    where
+    content = B.concat ["HTTP/1.1 ", statusMsg status, "\r\n\
+        \Server: Ketchup\r\n\
+        \Connection: keep-alive\r\n",heads,"\r\n\
+        \Transfer-Encoding: chunked\r\n\r\n"]
+    heads = B.concat $ map (\x -> B.concat [fst x, ": ", B.concat $ List.intersperse "," $ snd x]) headers
+
+
+chunk :: Socket -> B.Bytestring -> IO ()
+chunk handle value =
+    sendAll handle content
+    where
+    content = B.concat [B.pack $ showHex (length value) "",
+                        "\r\n", value, "\r\n"]
+
+endchunk :: Socket -> IO ()
+endchunk handle = sendAll handle "0\r\n\r\n"
--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
-all: example
+all: base example
+
+base:
+	cabal configure
+	cabal build
 
 example:
 	ghc example.hs
 
+install:
+	cabal install
+
 clean:
 	rm -rf ./example
 	rm -rf *.o */*.o *.hi */*.hi
+	cabal clean
