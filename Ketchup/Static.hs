3dcb70e0d4572b13c1c420f5b725d67ce18332b1
Hamcha <zikyky@gmail.com>
2015-02-15 13:55:59 +0100

    Do-less static

--- Ketchup/Static.hs
+++ Ketchup/Static.hs
@@ -15,23 +15,23 @@ import           System.Directory (doesFileExist)
 -- Takes a directory and returns a route
 static :: B.ByteString -- ^ Path to serve static files from
           -> Handler
-static folder hnd req = do
-    let path = B.concat [folder, uri req]
-    let sane = sanecheck path
-    let strPath = B.unpack path
-    doesExist <- doesFileExist strPath
-    case and [sane, doesExist] of
-        True  -> B.readFile strPath
-                 >>= sendReply hnd 200 [("Content-Type",[mime])]
-                 where
-                 mime = defaultMimeLookup $ decodeUtf8 fname
-                 fname = last $ B.split '/' path
-        False -> sendNotFound hnd
+static folder hnd req =
+    doesFileExist strPath
+    >>= \exists ->
+        case and [sanecheck path, exists] of
+            False -> sendNotFound hnd
+            True  -> B.readFile strPath
+                     >>= sendReply hnd 200 [("Content-Type",[mime])]
+                     where
+                     mime = defaultMimeLookup $ decodeUtf8 fname
+                     fname = last $ B.split '/' path
+    where
+    strPath = B.unpack path
+    path    = B.concat [folder, uri req]
 
 sanecheck :: B.ByteString -> Bool
 sanecheck url =
-    and checks
+    and [parentcheck]
     where
-    checks = [parentcheck]
     parentcheck = length (filter (== "..") pieces) < 1
     pieces = B.split '/' url
